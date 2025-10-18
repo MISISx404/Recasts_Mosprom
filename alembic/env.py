4951440084e018cc6f7741bd13673241d3013453
Gaveyn <crstalhopkin@gmail.com>
2025-10-19 02:06:00 +0500

    Update alembic con atring

--- alembic/env.py
+++ alembic/env.py
@@ -38,7 +38,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = "postgresql+psycopg2://recsys_user:recsys_pass@localhost:5433/recsys_db"
     context.configure(
         url=url,
         target_metadata=target_metadata,
