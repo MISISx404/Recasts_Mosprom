135827699903d34c4a3d88bef1d059426779a1d4
Gaveyn <crstalhopkin@gmail.com>
2025-10-19 05:29:09 +0500

    Update toxic classifier

--- app/toxic_analis.py
+++ app/toxic_analis.py
@@ -4,5 +4,7 @@ classifier = pipeline("text-classification", model="s-nlp/russian_toxicity_class
 
 def is_toxic_by_model(text, threshold = 0.6):
     result = classifier(text)[0]
-    print(f"score: {result['score']:.2f}, label: {result['label']}", result['score'])
-    return result['label'] == 'neutral' and float(result['score']) > threshold 
+    score = result['score']
+    label = result['label']
+    print(f"score: {score:.2f}, label: {label}")
+    return label == 'neutral' and float(score) > threshold 
