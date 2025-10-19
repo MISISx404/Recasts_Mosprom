e07b48b08ba96c0ff428ad4be68a89387163c22c
Gaveyn <crstalhopkin@gmail.com>
2025-10-19 05:12:26 +0500

    Update toxic classifier

--- app/toxic_analis.py
+++ app/toxic_analis.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 classifier = pipeline("text-classification", model="s-nlp/russian_toxicity_classifier")
 
-def is_toxic_by_model(text, threshold=0.6):
+def is_toxic_by_model(text, threshold = 0.6):
     result = classifier(text)[0]
     print(f"score: {result['score']:.2f}, label: {result['label']}")
-    return result['label'] == 'toxic' and result['score'] > threshold
+    return result['label'] == 'toxic' and result['score'] > threshold 
